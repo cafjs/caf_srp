e65ddeb7e808e8f75ad2a6a25179db95398395e9
Antonio Lain <antlai@cafjs.com>
2016-07-29 00:43:18 -0700

    Support eslint

--- util/newToken.js
+++ util/newToken.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
+'use strict';
 
 /**
  * Creates a signed token to identify the device or app instance.
  *
- * ./newToken.js --password=<change> --accountsURL=https://root-accounts.cafjs.com --caOwner=foo --caLocalName=ca1 --appLocalName=helloiot --appPublisher=root
+ * ./newToken.js --password=<change>
+ *     --accountsURL=https://root-accounts.cafjs.com --caOwner=foo
+ *     --caLocalName=ca1 --appLocalName=helloiot --appPublisher=root
  * Alternatively, use properties CAF_<all caps, name like above no spaces>,
  *  e.g., CAF_PASSWORD or CAF_APPLOCALNAME.
  *
@@ -15,14 +18,15 @@ var srpClient = require('../index.js').client;
 
 var newToken = function(settings, cb) {
     var spec = {
+        // eslint-disable-next-line
         log: function(x) { console.error(x);},
         securityClient: srpClient,
         accountsURL: settings.accountsURL,
         password: settings.password,
         from: settings.caOwner + '-' + settings.caLocalName,
 //        durationInSec: settings.durationInSec,
-        appLocalName : settings.appLocalName,
-        appPublisher : settings.appPublisher,
+        appLocalName: settings.appLocalName,
+        appPublisher: settings.appPublisher,
         unrestrictedToken: false
     };
 
@@ -30,8 +34,10 @@ var newToken = function(settings, cb) {
 
     tf.newToken(null, function(err, data) {
         if (err) {
+            // eslint-disable-next-line
             console.error('Cannot create token: ' + JSON.stringify(err));
         } else {
+            // eslint-disable-next-line
             console.log(data);
         }
         cb(err, data);
@@ -41,10 +47,11 @@ var newToken = function(settings, cb) {
 var argv = parseArgs(process.argv.slice(2));
 
 var prop = function(x, defaultValue) {
-    var result =  argv[x] || process.env['CAF_' + x.toUpperCase()] ||
+    var result = argv[x] || process.env['CAF_' + x.toUpperCase()] ||
             defaultValue;
     if (typeof result !== 'string') {
         var msg = 'Property ' + x + ' not defined';
+        // eslint-disable-next-line
         console.error(msg);
         throw new Error(msg);
     }
@@ -59,4 +66,3 @@ newToken({
     appLocalName: prop('appLocalName'),
     appPublisher: prop('appPublisher')
 }, function() {});
-
